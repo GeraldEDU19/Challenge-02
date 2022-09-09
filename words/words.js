4b63403c0e1dbcddaf25b376944451ec8d73a27e
GeraldEDU19 <110360484+GeraldEDU19@users.noreply.github.com>
2022-09-09 10:05:08 -0600

    e

--- words/words.js
+++ words/words.js
@@ -6,12 +6,17 @@ class words{
     addWord(word){
         this.wordsArray.push(word)
     }
+    getWordsArray(){
+        return this.wordsArray;
+    }
     
 }
 
 console.log("Hola")
-
+words = words();
+words.addWord("Hola")
+console.log(words.getWordsArray())
 
 function refresh(){
-    words = words();
+    
 }
