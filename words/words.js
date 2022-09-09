52062d7de7ae366e0e2bdcf61478f8c6b3ee4561
GeraldEDU19 <110360484+GeraldEDU19@users.noreply.github.com>
2022-09-09 09:40:20 -0600

    word 6

---
+++ words/words.js
@@ -0,0 +1,17 @@
+class words{
+
+    constructor(){
+        this.wordsArray =[];
+    }
+    addWord(word){
+        this.wordsArray.push(word)
+    }
+    
+}
+
+console.log("Hola")
+
+
+function refresh(){
+    words = words();
+}
